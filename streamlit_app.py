104eb5f8e5ec4431cd33efdb2d3f00cf1be16afe
TradeObjetivo <76456782+Lucas1391@users.noreply.github.com>
2023-04-29 00:17:28 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 #====================================Codigo Principal==================================================
-uploaded_file = st.file_uploader("Carregue o Arquivo!")
-
-if uploaded_file is not None:
-    for uploaded_file in uploaded_files:
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
+  
